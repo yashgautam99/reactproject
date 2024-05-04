ea260f60c20f6f400dde64aac3e63ff0a8f5e19d
Yash Gautam <yash99gautam@gmail.com>
2024-05-04 19:18:04 +0530

    Heading Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Heading from "./components/Heading";
 
 function App() {
   const [color, setColor] = useState("red");
@@ -6,72 +7,86 @@ function App() {
   return (
     <>
       <div
-        className=" w-full h-screen"
+        className=" w-full h-screen text-center flex justify-center"
         style={{ backgroundColor: color }}
-      ></div>
+      >
+        <Heading />
+        {/* <div className="text-center">Hello</div> */}
+      </div>
       <div className="fixed flex flex-wrap justify-center bottom-9 inset-x-0 px-4">
-        <div className="bg-slate-100 px-5 py-4 rounded-2xl flex flex-wrap justify-center shadow-lg  gap-3">
+        <div className="bg-indigo-100 px-5 py-4 rounded-2xl flex flex-wrap justify-center shadow-lg  gap-3">
           <button
+            onClick={() => setColor("red")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "red" }}
           >
             Red
           </button>
           <button
+            onClick={() => setColor("green")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "green" }}
           >
             Green
           </button>
           <button
+            onClick={() => setColor("pink")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "pink" }}
           >
             Pink
           </button>
           <button
+            onClick={() => setColor("Grey")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "Grey" }}
           >
             Grey
           </button>
           <button
+            onClick={() => setColor("purple")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "Purple" }}
           >
             Purple
           </button>
           <button
+            onClick={() => setColor("Gold")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "Gold" }}
           >
             Gold
           </button>
           <button
+            onClick={() => setColor("blue")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "blue" }}
           >
             Blue
           </button>
           <button
+            onClick={() => setColor("black")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none text-white"
             style={{ backgroundColor: "black" }}
           >
             Black
           </button>
           <button
+            onClick={() => setColor("Silver")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "Silver" }}
           >
             Silver
           </button>
           <button
+            onClick={() => setColor("brown")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "brown" }}
           >
             Brown
           </button>
           <button
+            onClick={() => setColor("blueviolet")}
             className=" px-4 py-2 rounded-3xl shadow-slate-950 outline-none"
             style={{ backgroundColor: "blueviolet" }}
           >
---
+++ src/components/Heading.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div className="font-bold text-2xl text-white w-3/5 p-14">
+      In this project, we utilize the power of React for building dynamic user
+      interfaces and Tailwind CSS for efficient styling. The main functionality
+      involves changing the background color of the page dynamically upon
+      clicking different buttons. React's component-based architecture allows us
+      to create reusable and maintainable UI components, while Tailwind CSS
+      streamlines the styling process with its utility-first approach, enabling
+      rapid prototyping and easy customization. By combining these technologies,
+      we achieve a modern, responsive, and visually appealing user experience,
+      with seamless color transitions enhancing user interaction.
+    </div>
+  );
+}
+
+export default Heading;
